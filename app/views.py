7b979e859538e064606f3e0910b0e15ff31af4ff
Jhonathan324 <158005754+Jhonathan324@users.noreply.github.com>
2025-07-09 08:54:37 -0300

    melhoria no front-end e no css geral

--- app/views.py
+++ app/views.py
@@ -6,7 +6,10 @@ from .models import *
 
 class IndexView(View):
     def get(self, request, *args, **kwargs):
-        return render(request, 'index.html')
+        jogos = Jogo.objects.all()
+        membros = MembroEquipe.objects.all()
+        tarefas = Tarefa.objects.all()
+        return render(request, 'index.html', {'jogos': jogos,'membros': membros,'tarefas': tarefas})
 
 
 # Jogo e Relacionados
